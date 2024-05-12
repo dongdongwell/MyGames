66dbc31ef08e7fb7f0bf0de9c2dda118afed39eb
dong <108759113@qq.com>
2024-05-12 19:42:16 +0800

    完成小鸟会动的机制

--- FlappyBird/const.py
+++ FlappyBird/const.py
@@ -1,11 +1,13 @@
 MAP_WIDTH = 284
 MAP_HEIGHT = 512
 
-BACKGROUND = 'images/background.png'
-GROUND = 'images/ground.png'
-PIPE_BODY = 'images/pipe_body.png'
-PIPE_END = 'images/pipe_end.png'
-PIPE = 'images/pipe.png'
-BIRD_WING_DOWN = 'images/bird_wing_down.png'
-BIRD_WING_UP = 'images/bird_wing_up.png'
-BIRD = 'images/bird.gif'
+BACKGROUND = 'FlappyBird/images/background.png'
+GROUND = 'FlappyBird/images/ground.png'
+PIPE_BODY = 'FlappyBird/images/pipe_body.png'
+PIPE_END = 'FlappyBird/images/pipe_end.png'
+PIPE = 'FlappyBird/images/pipe.png'
+BIRD_WING_DOWN = 'FlappyBird/images/bird_wing_down.png'
+BIRD_WING_UP = 'FlappyBird/images/bird_wing_up.png'
+BIRD = 'FlappyBird/images/bird.gif'
+
+FPS = 60
--- FlappyBird/main.py
+++ FlappyBird/main.py
@@ -2,12 +2,26 @@ import pygame
 import const
 pygame.init()
 
+# 定义帧率
+frame = 0
+
+clock = pygame.time.Clock()
 gameScreen = pygame.display.set_mode((const.MAP_WIDTH, const.MAP_HEIGHT))
+
 background = pygame.image.load(const.BACKGROUND)
 bird_wing_up = pygame.image.load(const.BIRD_WING_UP)
+bird_wing_down = pygame.image.load(const.BIRD_WING_DOWN)
 
 def draw_birds(x, y):
-    gameScreen.blit(bird_wing_up, (x, y))
+    global frame
+    if 0 <= frame <= 30:
+        gameScreen.blit(bird_wing_up, (x, y))
+        frame += 1
+    elif 30 < frame <= 60:
+        gameScreen.blit(bird_wing_down, (x, y))
+        frame += 1
+        if frame == 60:
+            frame = 0
 
 def gameLoop():
     while True:
@@ -20,6 +34,7 @@ def gameLoop():
                 return
 
         pygame.display.update()
+        clock.tick(const.FPS)
 
 if __name__ == '__main__':
     gameLoop()
