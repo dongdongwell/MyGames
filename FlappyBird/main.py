e7f1f908ff3899352e36ba15063e4c4a564701af
dong <108759113@qq.com>
2024-05-12 19:29:43 +0800

    显示小鸟

--- FlappyBird/main.py
+++ FlappyBird/main.py
@@ -4,10 +4,15 @@ pygame.init()
 
 gameScreen = pygame.display.set_mode((const.MAP_WIDTH, const.MAP_HEIGHT))
 background = pygame.image.load(const.BACKGROUND)
+bird_wing_up = pygame.image.load(const.BIRD_WING_UP)
+
+def draw_birds(x, y):
+    gameScreen.blit(bird_wing_up, (x, y))
 
 def gameLoop():
     while True:
         gameScreen.blit(background, (0, 0))
+        draw_birds(20, const.MAP_HEIGHT // 2)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
