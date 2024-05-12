4c9a7942e82847771e572b61e09f13282a9f9f75
dong <108759113@qq.com>
2024-05-12 19:22:45 +0800

    添加了点击退出按钮可以退出

--- FlappyBird/main.py
+++ FlappyBird/main.py
@@ -8,6 +8,12 @@ background = pygame.image.load(const.BACKGROUND)
 def gameLoop():
     while True:
         gameScreen.blit(background, (0, 0))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
         pygame.display.update()
 
 if __name__ == '__main__':
